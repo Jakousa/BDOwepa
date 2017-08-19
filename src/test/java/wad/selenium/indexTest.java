2ed2fb65f28f81ef1b47c9b170cc21006ee0eac8
Jakousa <japekohatcher93@hotmail.com>
2017-08-19 17:36:08 +0300

    Init tests

---
+++ src/test/java/wad/selenium/indexTest.java
@@ -0,0 +1,34 @@
+package wad.selenium;
+
+import org.fluentlenium.adapter.FluentTest;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class indexTest extends FluentTest {
+
+    public WebDriver webDriver = new HtmlUnitDriver();
+
+    @Override
+    public WebDriver getDefaultDriver() {
+        return webDriver;
+    }
+
+    @LocalServerPort
+    private Integer port;
+
+    @Test
+    public void openIndex() {
+        goTo("http://localhost:" + port + "/index");
+
+        assertTrue(pageSource().contains("TODO"));
+    }
+
+}
