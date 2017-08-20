33815cef6c26253c0fa7d49dd46d631a862fdaae
Jakousa <japekohatcher93@hotmail.com>
2017-08-20 18:07:34 +0300

    A bit better timer

--- src/main/java/wad/controller/DefaultController.java
+++ src/main/java/wad/controller/DefaultController.java
@@ -1,15 +1,22 @@
 package wad.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import wad.service.BossTimerService;
 
 @Controller
 @RequestMapping("*")
 public class DefaultController {
     
+    @Autowired
+    BossTimerService bossTimerService;
+    
     @RequestMapping(method = RequestMethod.GET)
-    public String view() {
+    public String view(Model model) {
+        model.addAttribute("timers", bossTimerService.getTimers());
         return "index";
     }
 }
