5d09ab75bfc2b5d5d03859ee555f0d120e4af3e9
Jakousa <japekohatcher93@hotmail.com>
2017-09-10 01:29:32 +0300

    Add bootstrap

--- src/main/resources/public/javascript/bosstimerlist.js
+++ src/main/resources/public/javascript/bosstimerlist.js
@@ -13,14 +13,18 @@ function displayTimer(timer) {
     var paragraph = document.createElement("p");
     var textArray = createTimerText(timer);
     for (i = 0; i < textArray.length; i++) {
-        paragraph.appendChild(document.createTextNode(textArray[i]));
         paragraph.appendChild(document.createElement("br"));
+        paragraph.appendChild(document.createTextNode(textArray[i]));
     }
     var children = document.getElementById("timers").children;
     for (var i = 0; i < children.length; i++) {
         var tableChild = children[i];
         if (tableChild.innerHTML.includes(timer.name)) {
-            document.getElementById("timers").replaceChild(paragraph, tableChild);
+            var row = document.createElement("div");
+            row.setAttribute("class", "col-6 col-lg-3");
+            row.appendChild(paragraph);
+            row.appendChild(createProgressBar(timer));
+            document.getElementById("timers").replaceChild(row, tableChild);
             return;
         }
     }
@@ -37,6 +41,37 @@ function createTimerText(timer) {
     } else {
         return ["(" + timer.name + ") ", "Starting in: " + timeFromStart + " Estimated to arrive in: " + timeFromEstimate, "Estimate: " + new Date(timer.spawnEstimated).toTimeString()];
     }
+}
+
+function createProgressBar(timer) {
+    var percent = makePercentagesFromTime(timer);
+    var progress = document.createElement("div");
+    progress.className = "progress";
+    var progressBar = document.createElement("div");
+    if (percent >= 99) {
+        progressBar.setAttribute("class", "progress-bar progress-bar-danger");
+    } else if (percent >= 75) {
+        progressBar.setAttribute("class", "progress-bar progress-bar-warning");
+    } else {
+        progressBar.setAttribute("class", "progress-bar progress-bar-success");
+    }
+    progressBar.setAttribute("role", "progressbar");
+    progressBar.setAttribute("aria-valuenow", percent);
+    progressBar.setAttribute("aria-valuemin", "0");
+    progressBar.setAttribute("aria-valuemax", "100");
+    progressBar.setAttribute("style", "width: " + percent + "%");
+    progress.appendChild(progressBar);
+    return progress;
+}
+
+function makePercentagesFromTime(timer) {
+    var lastTime = new Date(timer.lastSpawn);
+    var now = new Date();
+    var coming = new Date(timer.spawnStart);
+
+    var diff = coming - lastTime;
+    var atm = now - lastTime;
+    return Math.max(0, Math.min(Math.floor((atm / diff) * 100), 100));
 }
 
 function getTimeFrom(time) {
